9b43aa902e5a7e3b5f279240b3911dc5e18dcd79
junara <tepada@yahoo.co.jp>
2016-02-24 05:02:42 +0000

    error message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,21 @@
 class MessagesController < ApplicationController
   def index
-    @message = Message.new
+    @message = Message.new #create時に使う入れ物を用意している。
     # Messageを全て取得する。
     #複数形には意味があるのか？
-    @messages = Message.all
+    @messages = Message.all #viewで使うmessagesテーブルの内容を全て入れておく。
   end
   
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージ保存しました。'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージ保存しました。'
+    else
+      #メッセージのvalidationでエラーが出たとき
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
   
   private
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ActiveRecord::Base
+    # 「名前」は必須入力かつ20文字以内
+    validates :name , length: { maximum: 20 }, presence: true
+    # 「内容」は必須入力かつ2文字以上30文字以下
+    validates :body , length: { maximum: 30, minimum: 2} , presence: true
+
 end
