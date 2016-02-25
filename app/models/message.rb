cd96a357039e252d91eff904c3990568c72759e3
junara <tepada@yahoo.co.jp>
2016-02-25 08:06:27 +0000

    age validation add

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,6 +6,6 @@ class Message < ActiveRecord::Base
     validates :body ,  presence: { message: "は必須入力"} , length: { maximum: 30, minimum: 2 , message: "は2文字以上30文字以下"} 
 
     # 「年齢」は必須入力かつ0以上132以下。年齢の世界最高記録（122）+10=132
-    validates :age ,  presence: { message: "は必須入力"} , numericality: { only_integer: true, greater_than_or_equal_to: 0 ,less_than_or_equal_to: 132 , message: "は2文字以上30文字以下"}
+    validates :age ,  presence: { message: "は必須入力"} , numericality: { only_integer: true, greater_than_or_equal_to: 0 ,less_than_or_equal_to: 132 , message: "は0以上132以下"}
 
 end
